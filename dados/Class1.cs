192bc7f3502daccd3217cb69b053ff8d254db8cf
Fabio Freller <f.freller@gmail.com>
2017-11-01 18:56:57 -0200

    2

---
+++ dados/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dados
+{
+    public class Dados
+    {
+        public int banco { get; set; }
+        public int agencia { get; set; }
+        public int contacorrente { get; set; }
+        public double saldo { get; set; }
+        public double valor { get; set; }
+
+        public void Sacar(double valor)
+        {
+            saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            
+        }
+    }
+}
